e525f6f445cc02fa39f55c1efe655d43650440b0
Andre <andresionek91@gmail.com>
2021-09-30 23:03:09 +0100

    tenta commitar arquivo zuado

--- local_scripts/put_to_firehose.py
+++ local_scripts/put_to_firehose.py
@@ -17,6 +17,5 @@ def put_record(event):
 
 simulation = Simulation(user_pool_size=100, sessions_per_day=1000)
 events = simulation.run(duration_seconds=10000)
-
 for event in events:
     put_record(event)
